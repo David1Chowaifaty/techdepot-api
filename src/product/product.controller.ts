f69d605cfc7f5e794650e2167524c63bd3b61f07
Georgio <georgio.fayad1117@gmail.com>
2023-08-01 16:11:27 +0300

    create rateAProduct

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,6 +1,6 @@
 import {Response , Request} from "express"
 import {Connection} from "mysql2"
-import { GetAllCategories, GetProductByCategoryAndPrice, GetSingleProduct } from "./product.service";
+import { GetAllCategories, GetProductByCategoryAndPrice, GetSingleProduct, RateAProduct } from "./product.service";
 import { ZodError, z } from "zod";
 
 export async  function getAllCategories(req: Request,res: Response ){
@@ -46,6 +46,32 @@ export async function getProductByCategoryAndPrice (req: Request ,res: Response)
         const connection :Connection = req.app.get("connection")
         const result = await GetProductByCategoryAndPrice(connection , category , price)
     
+        res.send(result)
+    
+        
+    } catch (error) {
+        if(error instanceof ZodError){
+
+            res.status(405).send(error)
+        }
+        res.status(500).send(error)
+        
+    }
+}
+export async function rateAProduct(req: Request ,res: Response){
+    try {
+        const productScheme = z.object({
+            user_id:z.coerce.number(),
+            product_id:z.coerce.number(),
+            rating:z.coerce.number()
+        })
+        let {user_id , product_id , rating}=productScheme.parse(req.body)
+        if(rating>5 || rating<1){
+            res.status(400).send("Invalid rating");
+        }
+        
+        const connection :Connection = req.app.get("connection")
+        const result = await RateAProduct(connection , user_id , product_id,rating)
         res.send(result)
     
         
